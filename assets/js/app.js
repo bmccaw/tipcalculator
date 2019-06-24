ebafa0bae55d2c54fbd1019e81f01ddb5bb2444a
Green Man <brendan.mccaughey@gmail.com>
2019-06-24 14:57:00 -0400

    Intial file creation and page setup.

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+
+function calculateTip () {
+	//define variables
+	const billVal = document.getElementById(`bill-val`).value;
+	const tipPercent = document.getElementById(`tip-percent`).value;
+	const numPatrons = document.getElementById(`num-patrons`).value;
+	
+}
+});
