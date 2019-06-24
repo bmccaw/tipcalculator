c10346cee4a975b412e9f1ddd8111e1a7b13035c
Green Man <brendan.mccaughey@gmail.com>
2019-06-24 18:54:53 -0400

    Began adding validations and calc equation.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,10 +2,22 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 const calculateTip = () => {
 	//define variables
-	const billVal = document.getElementById(`bill-val`).value;
-	const tipPercent = document.getElementById(`tip-percent`).value;
-	const numPatrons = document.getElementById(`num-patrons`).value;
+	const billVal = document.querySelector(`#bill-val`).value;
+	const tipPercent = document.querySelector(`#tip-percent`).value;
+	const numPatrons = document.querySelector(`#num-patrons`).value;
 
-	
+	//validate inputs
+	if (billVal === "" || tipPercent == 0 || numPatrons <= 0) {
+		alert("Please enter a valid value.");
+		return;
+	}
+
+	//calculate the tip
+	const total = (billVal * tipPercent) / numPatrons;
 }
+
+document.querySelector(`#calc-tip`).addEventListener("click", event => {
+	calculateTip()
+})
+
 });
