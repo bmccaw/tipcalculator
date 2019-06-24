225d6c5199f2250e097c0525eff1dcbf5f5b92bc
Green Man <brendan.mccaughey@gmail.com>
2019-06-24 16:06:12 -0400

    Added additional form fields and submit button.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", (event) => {
 
-function calculateTip () {
+const calculateTip = () => {
 	//define variables
 	const billVal = document.getElementById(`bill-val`).value;
 	const tipPercent = document.getElementById(`tip-percent`).value;
 	const numPatrons = document.getElementById(`num-patrons`).value;
+
 	
 }
 });
