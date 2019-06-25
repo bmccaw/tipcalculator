bb471321d9a975501a0e0e9fa80310adcb4e3c15
Green Man <brendan.mccaughey@gmail.com>
2019-06-24 21:28:45 -0400

    Moved const definitons outside of calc function.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", (event) => {
 
-const calculateTip = () => {
 	//define variables
 	const billVal = document.querySelector(`#bill-val`).value;
 	const tipPercent = document.querySelector(`#tip-percent`).value;
 	const numPatrons = document.querySelector(`#num-patrons`).value;
+
+const calculateTip = () => {
+	
+
+	console.log(billVal);
+	console.log(tipPercent);
+	console.log(numPatrons);
 
 	//validate inputs
 	if (billVal === "" || tipPercent == 0 || numPatrons <= 0) {
@@ -15,6 +21,9 @@ const calculateTip = () => {
 	//calculate the tip
 	const total = (billVal * tipPercent) / numPatrons;
 }
+console.log(billVal);
+	console.log(tipPercent);
+	console.log(numPatrons);
 
 document.querySelector(`#calc-tip`).addEventListener("click", event => {
 	calculateTip()
