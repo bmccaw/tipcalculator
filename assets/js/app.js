02048fa1d904d86b2f01ea53920a5375de495832
Green Man <brendan.mccaughey@gmail.com>
2019-06-26 14:41:43 -0400

    Added regex to check that user only enters bill amount in the proper format.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded", (event) => {
 
-	
-
 const calculateTip = () => {
 	
 	//define variables
 	const billVal = document.querySelector(`#bill-val`).value;
 	const tipPercent = document.querySelector(`#tip-percent`).value;
 	const numPatrons = document.querySelector(`#num-patrons`).value;
+	let regex = new RegExp(/^(\d+)?([.]?\d{0,2})?$/);
 
 	console.log(billVal);
 	console.log(tipPercent);
@@ -19,12 +18,19 @@ const calculateTip = () => {
 		return;
 	}
 
+	//regex check to insure billVal can only be a number with one decimal point
+	if (regex.test(billVal) == false) {
+		alert("Please enter a valid value.");
+		return;
+	}
+
 	//calculate the tip
 	let total = (billVal * tipPercent) / numPatrons;
 	console.log(total);
 	total = Math.round(total * 100) / 100;
 	console.log(total);
 
+	//display the result
 	document.querySelector(`#tip-amount`).innerHTML = total;
 
 }
