24a6fb8a009190c5736a5f5842f49fde3414660e
Green Man <brendan.mccaughey@gmail.com>
2019-06-26 22:03:31 -0400

    Intial styling.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# tipcalculator
+# Tip Calculator
+
+This is a simple tip calculator app that I built for a coding challenge. It takes the bill amount, tip percentage, and number of people in your party and provides you with the tip amount each person owes.
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,40 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener("DOMContentLoaded", event => {
+  const calculateTip = () => {
+    //define variables
+    const billVal = document.querySelector(`#bill-val`).value;
+    const tipPercent = document.querySelector(`#tip-percent`).value;
+    const numPatrons = document.querySelector(`#num-patrons`).value;
+    const regex = new RegExp(/^(\d+)?([.]?\d{0,2})?$/);
 
-const calculateTip = () => {
-	
-	//define variables
-	const billVal = document.querySelector(`#bill-val`).value;
-	const tipPercent = document.querySelector(`#tip-percent`).value;
-	const numPatrons = document.querySelector(`#num-patrons`).value;
-	let regex = new RegExp(/^(\d+)?([.]?\d{0,2})?$/);
+    console.log(billVal);
+    console.log(tipPercent);
+    console.log(numPatrons);
 
-	console.log(billVal);
-	console.log(tipPercent);
-	console.log(numPatrons);
+    //validate inputs
+    if (billVal === "" || tipPercent == 0 || numPatrons <= 0) {
+      alert("Please enter a valid value.");
+      return;
+    }
 
-	//validate inputs
-	if (billVal === "" || tipPercent == 0 || numPatrons <= 0) {
-		alert("Please enter a valid value.");
-		return;
-	}
+    //regex check to insure billVal can only be a number with one decimal point
+    if (regex.test(billVal) == false) {
+      alert("Please enter a valid bill amount.");
+      return;
+    }
 
-	//regex check to insure billVal can only be a number with one decimal point
-	if (regex.test(billVal) == false) {
-		alert("Please enter a valid value.");
-		return;
-	}
+    //calculate the tip
+    let total = (billVal * tipPercent) / numPatrons;
+    console.log(total);
+    total = Math.round(total * 100) / 100;
+    console.log(total);
 
-	//calculate the tip
-	let total = (billVal * tipPercent) / numPatrons;
-	console.log(total);
-	total = Math.round(total * 100) / 100;
-	console.log(total);
-
-	//display the result
+    //display the result
 	document.querySelector(`#tip-amount`).innerHTML = total;
+	document.querySelector(`#result`).style.visibility = "visible";
+  };
 
-}
-
-document.querySelector(`#calc-tip`).addEventListener("click", event => {
-	calculateTip()
-})
+  //run the function on button click
+  document.querySelector(`#calc-tip`).addEventListener("click", event => {
+    calculateTip();
+  });
 });
