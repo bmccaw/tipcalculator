cb82dc1586c3dbf60f0b60d0226bce78ce9b46b6
Green Man <brendan.mccaughey@gmail.com>
2019-06-28 12:17:44 -0400

    Split up validations to correspond with each input.

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,30 +11,41 @@ document.addEventListener("DOMContentLoaded", event => {
     console.log(numPatrons);
 
     //validate inputs
-    if (billVal === "" || tipPercent == 0 || numPatrons <= 0) {
-      alert("Please enter a valid value.");
-      return;
+    if (billVal === "") {
+		document.querySelector(`#err-bill`).style.visibility = "visible";
+	}
+	
+	//regex check to insure billVal can only be a number with one decimal point
+    else if (regex.test(billVal) == false) {
+		document.querySelector(`#err-bill`).style.visibility = "visible";
     }
 
-    //regex check to insure billVal can only be a number with one decimal point
-    if (regex.test(billVal) == false) {
-      alert("Please enter a valid bill amount.");
-      return;
+	else if (tipPercent == 0) {
+		document.querySelector(`#err-percent`).style.visibility = "visible";
     }
+
+	else if (numPatrons <= 0) {
+		document.querySelector(`#err-patron`).style.visibility = "visible";
+      return;
+    } else {
 
     //calculate the tip
     let total = (billVal * tipPercent) / numPatrons;
-	console.log(total);
-	//round the total to the nearest two decimal places
+    console.log(total);
+    //round the total to the nearest two decimal places
     total = Math.round(total * 100) / 100;
-	console.log(total);
-	//insure that the result always has two digits following the decimal point
-	total = total.toFixed(2);
-	console.log(total);
+    console.log(total);
+    //insure that the result always has two digits following the decimal point
+    total = total.toFixed(2);
+    console.log(total);
 
     //display the result
-	document.querySelector(`#tip-amount`).innerHTML = total;
+    document.querySelector(`#tip-amount`).innerHTML = total;
 	document.querySelector(`#result`).style.visibility = "visible";
+	document.querySelector(`#err-bill`).style.visibility = "hidden";
+	document.querySelector(`#err-percent`).style.visibility = "hidden";
+	document.querySelector(`#err-patron`).style.visibility = "hidden";
+	}
   };
 
   //run the function on button click
